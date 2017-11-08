e05b6909b7c25d2984697996cf1035e5dbbe38e8
Nick Barrett <nick@edited.com>
2017-11-08 11:17:15 +0000

    Setup details.

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ if __name__ == '__main__':
     setup(
         version='0.1',
         name='pyinfra-kubernetes',
+        author='EDITED',
+        author_email='engineering@edited.com',
+        url='https://github.com/EDITD/pyinfra-kubernetes',
+        license='MIT',
         description='Install & bootstrap Kubernetes clusters with pyinfra.',
         packages=find_packages(),
         install_requires=('pyinfra>=0.5'),
